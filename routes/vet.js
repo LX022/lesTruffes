ffe9c16fdf75a8cc3c5111ead4c537e54484aa99
NadineWaelti <nadine.waelti@students.hevs.ch>
2019-05-26 10:05:30 +0200

    Update vet avec prise en compte du lieu

--- routes/vet.js
+++ routes/vet.js
@@ -4,25 +4,30 @@ var router = express.Router();
 
 /* GET vet page. */
 router.get('/', async function(req, res, next) {
-
-
+    //Recherche du vétérinaire à afficher
     let id = req.query.idVeterinaire;
     let veto = await models.veterinaire.findByPk(id);
-    let nom = veto.nomV +" " + veto.prenomV;
 
-    res.render('vet', { title: nom, veto : veto });        //Page title
+    //Recherche le lieu
+    let lieu = await models.lieu.findByPk(veto.idLieu);
+
+    let nom = veto.nomV +" " + veto.prenomV;
+    let lieux = await models.lieu.findAll({order: [['ville', 'ASC']]});
+    res.render('vet', { title: nom, veto : veto, lieux:lieux, lieu:lieu });   //Page title
 });
 
 
 /* UPDATE vet page. */
 router.post('/', async function (req, res) {
-    console.log(req.body.idVeterinaire);
-    //idLieu: req.body.idLieu à updater et faire le lien avec la table lieu, si n'existe pas crée
-    await models.veterinaire.update({nomV: req.body.nomV, prenomV:req.body.prenomV, cliniqueV: req.body.cliniqueV, rueV: req.body.rueV, specialiteV: req.body.specialiteV, commentaireV: req.body.commentaireV, tarifs: req.body.tarifs, bloque: req.body.bloque}, {where: {idVeterinaire: req.body.idVeterinaire}});
+    await models.veterinaire.update({idLieu:req.body.idLieu, nomV: req.body.nomV, prenomV:req.body.prenomV, cliniqueV: req.body.cliniqueV, rueV: req.body.rueV, specialiteV: req.body.specialiteV, commentaireV: req.body.commentaireV, tarifs: req.body.tarifs, bloque: req.body.bloque}, {where: {idVeterinaire: req.body.idVeterinaire}});
     let veto = await models.veterinaire.findByPk(req.body.idVeterinaire);
     let nom = veto.nomV +" " + veto.prenomV;
 
-    res.render('vet', { title: nom, veto : veto });        //Page title
+    //Recherche le lieu
+    let lieu = await models.lieu.findByPk(veto.idLieu);
+
+    let lieux = await models.lieu.findAll({order: [['ville', 'ASC']]});
+    res.render('vet', { title: nom, veto : veto , lieux:lieux, lieu:lieu});        //Page title
 
 });
 
