0501930e145577023594e1c64938e47e948c4a13
Gab <thinkys@live.com>
2019-05-26 22:24:19 +0200

    fix bugs after mergin with Nadine's branch

---
+++ routes/listeFA.js
@@ -0,0 +1,13 @@
+var models  = require('../models');
+var express = require('express');
+var router = express.Router();
+
+/* GET dogs page. */
+router.get('/', async function(req, res, next) {
+
+    let animals_has_FA = await models.Animal_has_FA.findAll();
+
+    res.render('dogs', { title: 'Nos truffes à adopter', dogs : dogs , animals_has_FA:animals_has_FA});        //Page title
+});
+
+module.exports = router;
