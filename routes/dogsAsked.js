15a6863024e239dbac155d61d54e00e11626cae1
NadineWaelti <nadine.waelti@students.hevs.ch>
2019-05-28 18:53:35 +0200

    Préparation des routes

--- routes/dogsAsked.js
+++ routes/dogsAsked.js
@@ -1,9 +1,37 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
-/* GET dogsAsked page. */
-router.get('/', function(req, res, next) {
-    res.render('dogsAsked', { title: "Gestion des adoptions" });        //Page title
+/* GET dogsAdmin page. */
+router.get('/', async function (req, res, next) {
+
+    //Si on est sûr que le lien existe, on le destroy
+    if(req.query.idmydogasked!==undefined && req.query.idmydogasked !==null && req.query.idPersonne!==undefined && req.query.idPersonne !==null){
+        await models.animalAskedAdoptant.destroy({where:{idAnimal:req.query.idmydogasked, idPersonne:req.body.idPersonne}});
+    }
+
+    //Liste des liens avec le nom des chiens et des personnes (include)
+
+    //Calcule le plus grand id de la table
+
+
+    res.render('dogsAdmin', {title: 'Gestion des chiens'});        //Page title
+});
+
+
+
+/* POST dogsFA page. */
+router.post('/', async function (req, res) {
+
+    //créer une association dog/adoptant
+
+    //récupérer la liste des liens à jour
+
+
+    //id max de la table
+
+    res.render('dogsAdmin', {title: 'Gestion des chiens'});        //Page title
+
 });
 
 module.exports = router;
--- routes/dogsFA.js
+++ routes/dogsFA.js
@@ -1,9 +1,37 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
-/* GET dogsFA page. */
-router.get('/', function(req, res, next) {
-    res.render('dogsFA', { title: "Chiens en famille d'accueil" });        //Page title
+/* GET dogsAdmin page. */
+router.get('/', async function (req, res, next) {
+
+    //Si on est sûr que le lien existe, on le destroy
+    if(req.query.idmydogfa!==undefined && req.query.idmydogfa !==null){
+        await models.animalHasFa.destroy({where:{idAnimal:req.query.idmydogfa, idPersonne:req.body.idPersonne}});
+    }
+
+    //Liste des liens avec le nom des chiens et des personnes (include)
+
+    //Calcule le plus grand id de la table
+
+
+    res.render('dogsAdmin', {title: 'Gestion des chiens'});        //Page title
+});
+
+
+
+/* POST dogsFA page. */
+router.post('/', async function (req, res) {
+
+    //créer une association dog/FA
+
+    //récupérer la liste des liens à jour
+
+
+    //id max de la table
+
+    res.render('dogsAdmin', {title: 'Gestion des chiens'});        //Page title
+
 });
 
 module.exports = router;
