8bdad59804b2cd89fbcd9e57493fe6542b353d07
NadineWaelti <nadine.waelti@students.hevs.ch>
2019-05-28 20:52:13 +0200

    Amélioré une fonction dans vet pour le lieu

--- routes/vet.js
+++ routes/vet.js
@@ -8,31 +8,40 @@ router.get('/', async function(req, res, next) {
     let id = req.query.idVeterinaire;
     let veto = await models.veterinaire.findByPk(id);
 
-    //Recherche le lieu
+    //Recherche le lieu du véto et les lieux en général
     let lieu = await models.lieu.findByPk(veto.idLieu);
-
-    let nom = veto.nomV +" " + veto.prenomV;
+    let pays = await models.pays.findAll();
     let lieux = await models.lieu.findAll({order: [['ville', 'ASC']]});
-    res.render('vet', { title: nom, veto : veto, lieux:lieux, lieu:lieu });   //Page title
+
+    //titre
+    let nom = veto.nomV +" " + veto.prenomV;
+
+    res.render('vet', { title: nom, veto : veto, lieux:lieux, lieu:lieu, pays:pays });   //Page title
 });
 
 
 /* UPDATE vet page. */
 router.post('/', async function (req, res) {
-    if(req.body.idLieu==' '){
-        let lieu = null;
-        await models.veterinaire.update({idLieu:lieu, nomV: req.body.nomV, prenomV:req.body.prenomV, cliniqueV: req.body.cliniqueV, rueV: req.body.rueV, specialiteV: req.body.specialiteV, commentaireV: req.body.commentaireV, tarifs: req.body.tarifs, bloque: req.body.bloque}, {where: {idVeterinaire: req.body.idVeterinaire}});
+
+    //UPDATE : si le lieu est vide alors remettre la valeur null
+    if(req.body.idLieu==='' && req.body.idVeterinaire.idLieu!==null){
+        await models.veterinaire.update({nomV: req.body.nomV, prenomV:req.body.prenomV, cliniqueV: req.body.cliniqueV, rueV: req.body.rueV, specialiteV: req.body.specialiteV, commentaireV: req.body.commentaireV, tarifs: req.body.tarifs, bloque: req.body.bloque}, {where: {idVeterinaire: req.body.idVeterinaire}});
     }else{
         await models.veterinaire.update({idLieu:req.body.idLieu, nomV: req.body.nomV, prenomV:req.body.prenomV, cliniqueV: req.body.cliniqueV, rueV: req.body.rueV, specialiteV: req.body.specialiteV, commentaireV: req.body.commentaireV, tarifs: req.body.tarifs, bloque: req.body.bloque}, {where: {idVeterinaire: req.body.idVeterinaire}});
     }
+
+    //recherche du vétérinaire màj
     let veto = await models.veterinaire.findByPk(req.body.idVeterinaire);
+
+    //titre
     let nom = veto.nomV +" " + veto.prenomV;
 
-    //Recherche le lieu
+    //Recherche le lieu du véto et les lieux en général
     let lieu = await models.lieu.findByPk(veto.idLieu);
-
+    let pays = await models.pays.findAll();
     let lieux = await models.lieu.findAll({order: [['ville', 'ASC']]});
-    res.render('vet', { title: nom, veto : veto , lieux:lieux, lieu:lieu});        //Page title
+
+    res.render('vet', { title: nom, veto : veto , lieux:lieux, lieu:lieu, pays:pays});        //Page title
 
 });
 
