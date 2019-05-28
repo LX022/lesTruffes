6ac508a75382adf20a54b17bb60ac3e2d17785ea
Nicolas Solioz <nicolas.solioz@students.hevs.ch>
2019-05-28 21:04:30 +0200

    devenir FA : avec un post, on peut envoyer a la boite gmail les infos de la personne qui veut devenir une famille d'accueil

--- routes/formulaireFA.js
+++ routes/formulaireFA.js
@@ -1,11 +1,118 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var nodemailer = require("nodemailer");
+
 
 
 /* GET fa page. */
 router.get('/', function(req, res, next) {
     res.render('formulaireFA', { title: "Devenir famille d'accueil"});
 });
+
+router.post('/', function(req, res, next) {
+
+    var lastname = req.body.lastname;
+    var firstname = req.body.firstname;
+    var email = req.body.email;
+    var facebook = req.body.facebook;
+    var telephone = req.body.telephone;
+    var adresse = req.body.adresse;
+    var npa = req.body.npa;
+    var localite = req.body.localite;
+    var datenaissance = req.body.datenaissance;
+    var espece = req.body.espece;
+    var sexe = req.body.sexe;
+    var ageduchien = req.body.ageduchien;
+    var taille = req.body.taille;
+    var jouradoption = req.body.jouradoption;
+
+    var logement = "Appartement";
+    if(req.body.typelogementMaison == true)
+        logement = "Maison";
+
+    var surfacelogement = req.body.surfacelogement;
+
+    var j = "sans";
+    if(req.body.jOui == true)
+        j = "oui";
+
+    if(req.body.jNon == true)
+        j = "non";
+
+    var enfant = "non";
+    if(req.body.enfantOui == true)
+        enfant = "oui";
+
+    var animaux = "non";
+    if(req.body.animauxOui == true)
+        animaux = "oui";
+
+    var propre = "non";
+    if(req.body.propreOui == true)
+        propre = "oui";
+
+    var maltraitance = "non";
+    if(req.body.maltraitanceOui == true)
+        maltraitance = "oui";
+
+    var soins = "non";
+    if(req.body.soinsOui == true)
+        soins = "oui";
+
+    var autreinfo = req.body.autreinfo;
+
+    var message =
+        "Coordonnées de la personne : " + "\n" +
+        "**********************************************************" + "\n" +
+        "Firstname : " + firstname + "\n" +
+        "Lastname : " + lastname  + "\n" +
+        "Email : " + email  + "\n" +
+        "Facebook : " + facebook + "\n" +
+        "Telephone : " + telephone + "\n" +
+        "Adresse : " + adresse + ", " + npa + "-" + localite + "\n" +
+        "Date de naissance : " + datenaissance + "\n\n" +
+        "Type de chiens désirés : " + "\n" +
+        "**********************************************************" + "\n" +
+        "Espèce : " + espece + "\n" +
+        "Sexe : " + sexe +  + "\n" +
+        "Age du chien : " + ageduchien + "\n" +
+        "Taille du chien : " + taille + "\n" +
+        "Jour de l'adoption : " + jouradoption + "\n\n" +
+        "Type d'environnement : " + "\n" +
+        "**********************************************************" + "\n" +
+        "Logement : " + logement + "\n" +
+        "Surface du logement : " + surfacelogement + "\n" +
+        "Présence d'enfants : " + enfant + "\n" +
+        "Présence d'autres animaux : " + animaux + "\n" +
+        "Entraînement d'animal non-propre : " + propre + "\n" +
+        "Accepte les animaux maltraités : " + maltraitance + "\n" +
+        "Donne des soins : " + soins + "\n\n" +
+        "Informations supplémentaires : " + "\n" +
+        "**********************************************************" + "\n" +
+        autreinfo;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
+        auth: {
+            user: 'lestruffesdesierre@gmail.com',
+            pass: 'Le$Truffes_396O'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let info = transporter.sendMail({
+        from: '"Les Truffes" <' + email + '>',
+        to: "lestruffesdesierre@gmail.com",
+        subject: "Email from " + lastname + " " + firstname + " : " + email + " @ Les Truffes",
+        text: message,
+    });
+
+    res.redirect('contact');
+})
 
 module.exports = router;
