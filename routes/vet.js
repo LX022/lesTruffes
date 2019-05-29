b10e531c496480fd997dda6ff8a842247facbf90
NadineWaelti <nadine.waelti@students.hevs.ch>
2019-05-29 23:58:37 +0200

    delete lieu en cours

--- routes/vet.js
+++ routes/vet.js
@@ -4,10 +4,15 @@ var router = express.Router();
 
 /* GET vet page. */
 router.get('/', async function(req, res, next) {
+
     //Recherche du vétérinaire à afficher
     let id = req.query.idVeterinaire;
     let veto = await models.veterinaire.findByPk(id);
 
+    //DELETE lieu
+    if(req.body.deleteLieu!==undefined){
+        await models.lieu.destroy({where:{idLieu:req.body.deleteLieu}});
+    }
 
     //Recherche le lieu du véto et les lieux en général
     let lieu = await models.lieu.findByPk(veto.idLieu);
@@ -50,6 +55,7 @@ router.post('/', async function (req, res) {
     }else{
         await models.veterinaire.update({idLieu:req.body.idLieu, nomV: req.body.nomV, prenomV:req.body.prenomV, cliniqueV: req.body.cliniqueV, rueV: req.body.rueV, specialiteV: req.body.specialiteV, commentaireV: req.body.commentaireV, tarifs: req.body.tarifs, bloque: req.body.bloque}, {where: {idVeterinaire: req.body.idVeterinaire}});
     }
+
 
     //recherche du vétérinaire màj
     let veto = await models.veterinaire.findByPk(req.body.idVeterinaire);
