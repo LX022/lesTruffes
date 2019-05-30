a5fc6b58ded9bb76238073724718cb15ea0b0014
NadineWaelti <nadine.waelti@students.hevs.ch>
2019-05-30 10:18:22 +0200

    Lieu par défaut non supprimable

--- routes/vet.js
+++ routes/vet.js
@@ -5,14 +5,18 @@ var router = express.Router();
 /* GET vet page. */
 router.get('/', async function(req, res, next) {
 
+    //Créer un lieu ? s'il n'existe pas
+    let lieuDefaut = await models.lieu.findAll({where:{codePostal:0}});
+    if(lieuDefaut<1) {
+        let lieuInconnu = await models.lieu.create({idLieu: 0, codePostal: 0, ville: "?",idPays:0});
+    }
+
     //Recherche du vétérinaire à afficher
     let id = req.query.idVeterinaire;
     let veto = await models.veterinaire.findByPk(id);
 
-    //DELETE lieu
-    if(req.body.deleteLieu!==undefined){
-        await models.lieu.destroy({where:{idLieu:req.body.deleteLieu}});
-    }
+    console.log(id);
+
 
     //Recherche le lieu du véto et les lieux en général
     let lieu = await models.lieu.findByPk(veto.idLieu);
@@ -41,7 +45,7 @@ router.post('/', async function (req, res) {
     //INSERT lieu
     //Si la Suisse n'existe pas l'inventer
     let laSuisse = await models.pays.findAll({where:{codePays:0}});
-    console.log(laSuisse.length.toString());
+
     if(laSuisse.length<1){
         laSuisse = await models.pays.create({idPays:0, codePays:0, alpha2:"CH", alpha3:"CH", nomEnGb:"Suisse", nomPays:"Suisse" });
     }
@@ -49,7 +53,13 @@ router.post('/', async function (req, res) {
         await models.lieu.create({idLieu:req.body.insertIdLieu, codePostal:req.body.insertCP, ville:req.body.insertVille, idPays:0});
     }
 
-    //UPDATE : si le lieu est vide alors remettre la valeur null
+    //Créer un lieu ? s'il n'existe pas
+    let lieuDefaut = await models.lieu.findAll({where:{codePostal:0}});
+    if(lieuDefaut<1) {
+        let lieuInconnu = await models.lieu.create({idLieu: 0, codePostal: 0, ville: "?",idPays:0});
+    }
+
+    //UPDATE : si le lieu est vide
     if(req.body.idLieu==='' && req.body.idVeterinaire.idLieu!==null){
         await models.veterinaire.update({nomV: req.body.nomV, prenomV:req.body.prenomV, cliniqueV: req.body.cliniqueV, rueV: req.body.rueV, specialiteV: req.body.specialiteV, commentaireV: req.body.commentaireV, tarifs: req.body.tarifs, bloque: req.body.bloque}, {where: {idVeterinaire: req.body.idVeterinaire}});
     }else{
