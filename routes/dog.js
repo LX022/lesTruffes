a5368ce84b0d54b153fe9456207b02c5794b4284
Yann <yann.cla@gmail.com>
2019-05-31 17:25:00 +0200

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var fs = require('fs');
 
 /* GET dog page. */
 router.get('/', async function (req, res, next) {
 
-
+   let directory = '../public/images/animals/';
    let id = req.query.idAnimal;
+    let x = 0;
+   let dog = await models.Animal.findByPk(id);
+   let nom = dog.nomAnimal;
 
-    let dog = await models.Animal.findByPk(id);
-    let nom = dog.nomAnimal;
+   fs.readdir(directory, (err, files) => {
+        files.forEach(file => {
+            console.log(file.charAt(9))
+            x++;
 
+        });
+    });
+
+    console.log("-------------______________________________------------------------_____________________--- "+ x)
     res.render('dog', {title: nom,dog : dog});        //Page title
 
 });
