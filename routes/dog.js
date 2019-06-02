c460f2af63dc3dea20da21a9819e24a3a1d75b54
Yann <yann.cla@gmail.com>
2019-06-02 17:08:38 +0200

    affichage des photos supplémentaires des chiens

--- routes/dog.js
+++ routes/dog.js
@@ -6,22 +6,26 @@ var fs = require('fs');
 /* GET dog page. */
 router.get('/', async function (req, res, next) {
 
-   let directory = '../public/images/animals/';
-   let id = req.query.idAnimal;
+    let directory = '../public/images/animals/';
+    let id = req.query.idAnimal;
     let x = 0;
-   let dog = await models.Animal.findByPk(id);
-   let nom = dog.nomAnimal;
+    let dog = await models.animal.findByPk(id);
+    let nom = dog.nomAnimal;
+   // let dogImages =[];
+   // let idLenght = dog.idAnimal.length;
 
-   fs.readdir(directory, (err, files) => {
-        files.forEach(file => {
-            console.log(file.charAt(9))
-            x++;
-
-        });
-    });
-
-    console.log("-------------______________________________------------------------_____________________--- "+ x)
-    res.render('dog', {title: nom,dog : dog});        //Page title
+  //  if (dog.nomAnimal !== null) {
+  //      let nomLength = dog.nomAnimal.length;
+  //      fs.readdir(directory, (err, files) => {
+  //          files.forEach(file => {
+   //             if(file.indexOf(nomLength+1)==dog.idAnimal){
+    //                dogImages[x]=file;
+    //                x++
+     //           }
+     //       });
+       // });
+   // }
+    res.render('dog', {title: nom, dog: dog});        //Page title
 
 });
 
