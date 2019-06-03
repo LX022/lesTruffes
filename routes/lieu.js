45b6de0acd9d37c6607b7a95a62d3313ffcf99c1
NadineWaelti <nadine.waelti@students.hevs.ch>
2019-06-03 21:16:12 +0200

    delete lieu okay

--- routes/lieu.js
+++ routes/lieu.js
@@ -6,25 +6,13 @@ var router = express.Router();
 router.post('/', async function(req, res, next) {
 
     //DELETE
-    if (req.body.deleteLieu <1) {
-        return res.render('lieu', {title: 'Lieu par défaut non supprimable'});        //Page title
-    }
 
-    if(req.body.deleteLieu!==undefined && req.body.deleteLieu>0) {
-
-            let test = await models.veterinaire.findAll({where: {idLieu: req.body.deleteLieu}});
-            if (test.length > 0) {
-
-                await models.veterinaire.update({idLieu: 0}, {where: {idLieu: req.body.deleteLieu}});
+    if(req.body.deleteLieu!==undefined ) {
+                await models.veterinaire.update({idLieu: null}, {where: {idLieu: req.body.deleteLieu}});
                 await models.lieu.destroy({where: {idLieu: req.body.deleteLieu}});
                 return res.render('lieu', {title: 'Lieu supprimé'});     //Page title
-            } else {
-                await models.lieu.destroy({where: {idLieu: req.body.deleteLieu}});
-                return res.render('lieu', {title: 'Lieu supprimé'});   //Page title
-
-
-        }
     }
+
 
     res.render('lieu', { title: 'Lieu bien supprimé'});        //Page title
 });
