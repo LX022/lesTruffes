e7eae62576f0b3ed7f87d0a4a6d3bb908d4fd95a
NadineWaelti <nadine.waelti@students.hevs.ch>
2019-06-09 01:59:02 +0200

    nouveau layout

--- routes/livredor.js
+++ routes/livredor.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET adoption page. */
 router.get('/', async function(req, res, next) {
-    res.render('livredor', { title: "Livre d'or" });        //Page title
+    res.render('livredor', { title: "Souvenirs" });        //Page title
 });
 
 module.exports = router;
