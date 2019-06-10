6ee8141a6817060dd1aabb015298e037c771fe9c
Bornatch <valentin.bornatici@gmail.com>
2019-06-10 11:58:52 +0200

    update

--- routes/about.js
+++ routes/about.js
@@ -4,8 +4,6 @@ var router = express.Router();
 /* GET about page. */
 router.get('/', function(req, res, next) {
 
-
-
     res.render('about', { title: 'Qui sommes-nous ?' , user:req.session.username});        //Page title
 
 });
